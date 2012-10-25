7cf0d3648cfccd4f29713ad0e11f6823f26a7f0b
s29zhu <shashazhu1989@gmail.com>
2012-10-24 20:43:05 -0400

    branches

---
+++ 1.py
@@ -0,0 +1,14 @@
+#!include /usr/env/python
+
+import math
+if __name__ == '__main__':
+	threeSum = 0
+	fiveSum = 0
+	fifteenSum = 0 
+	summ = 0
+	threeSum = 3*math.floor(1000/3)*(math.floor(1000/3)+1)/2
+	fiveSum = 5*math.floor(999/5)*(math.floor(999/5)+1)/2
+	fifteenSum = 15*math.floor(1000/15)*(math.floor(1000/15)+1)/2
+	summ = threeSum + fiveSum - fifteenSum
+	print threeSum, fiveSum, fifteenSum, summ
+	
